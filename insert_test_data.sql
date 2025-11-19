81042fa1238a4f0dc2e50e14449234bd9a0a87fa
lokekiri <lokello16@gmail.com>
2025-11-19 21:27:33 +0000

    Added default gold/smiths user to insert_test_data.sql

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -3,3 +3,6 @@
 USE berties_books;
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
+
+INSERT INTO users (username, firstname, lastname, email, hashedPassword)
+VALUES ('gold', 'Gold', 'Smiths', 'gold@example.com', '$2b$10$EHc6m5HH.Nr7jIQ6gUR9h.DQogIFLlpbp4LV10DTSpKxl1x/JDFMW');
